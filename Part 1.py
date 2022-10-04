0ab157ea64384d6b5bfb75eaea5e9017485f7649
Куликова Эмилия <mily123129@gmail.com>
2022-10-04 21:39:05 +0300

    проверка работы

--- Part 1.py
+++ Part 1.py
@@ -2,4 +2,6 @@ name = input("Введите имя ")
 surname = input("Введите фамилию ")
 street = input("Введите улицу ")
 number = input("Введите номер ")
-print(name,surname,street,number,sep=", ", end= ".")
+print(name, surname,
+street, number,
+sep = ", ", end = ".")
