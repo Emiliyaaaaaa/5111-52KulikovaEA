0880a776bc183885db992a564907e34b001bbc3e
Куликова Эмилия <mily123129@gmail.com>
2022-10-25 18:45:53 +0300

    проверка работы

--- Task 4/Part 2.py
+++ Task 4/Part 2.py
@@ -1,17 +1,23 @@
 # Cоздание словаря
 my_dictionary = {}
-key = 0
-while True:
-    value = input("Введите слово ")
-    # При отсутствии значения остановка цикла
-    if not value:
-        break
-    # Проверка вхождения значения в словарь
-    elif value not in my_dictionary.values():
-        key += 1
-        # Добавление в словарь пары ключ - значение
-        my_dictionary.update({key: value})
-# Переменная с доступом всех значений словаря
-m = my_dictionary.values()
+# Создание списка
+my_list = []
+key = input("Введите слово ")
+while key != "":
+    value = 0
+    # Добавление введенного слова в список
+    my_list.append(key)
+    for key in my_list:
+        # Если слово повторяется, то значение увеличивается на один
+        if key == key in my_list:
+            value += 1
+        else:
+            value = 1
+    # Добавление в словарь пары ключ - значение
+    my_dictionary.update({key: value})
+    key = input("Введите слово ")
+print(my_dictionary)
+# Переменная с доступом всех ключей словаря
+m = my_dictionary.keys()
 # Распаковка значений
 print(*m)
